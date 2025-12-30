db2849667eed164c4523eb9111bcf72e2c2acb85
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2025-12-30 22:10:32 +0530

    Time: 1 ms (44.89%) | Memory: 43.6 MB (17.05%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.java
+++ 870-magic-squares-in-grid/magic-squares-in-grid.java
@@ -1,42 +1,35 @@
 class Solution {
-
-    public int numMagicSquaresInside(int[][] grid) {
-        int ans = 0;
-        int m = grid.length;
-        int n = grid[0].length;
-        for (int row = 0; row + 2 < m; row++) {
-            for (int col = 0; col + 2 < n; col++) {
-                if (isMagicSquare(grid, row, col)) {
-                    ans++;
-                }
+    public boolean isMagicSquare(int[][] grid,int r,int c){
+        int sum = grid[r][c] + grid[r][c+1] + grid[r][c+2];
+        HashSet<Integer> set = new HashSet<>();
+ 
+        for(int i=0;i<3;i++){ // 1 - 9 checking
+            for(int j=0;j<3;j++){
+                int num = grid[r+i][c+j];
+                if(num<1 || num>9 || set.contains(num)) return false;
+                set.add(num);
             }
         }
-        return ans;
-    }
-
-    private boolean isMagicSquare(int[][] grid, int row, int col) {
-        // The sequences are each repeated twice to account for
-        // the different possible starting points of the sequence
-        // in the magic square
-        String sequence = "2943816729438167";
-        String sequenceReversed = "7618349276183492";
-
-        StringBuilder border = new StringBuilder();
-        // Flattened indices for bordering elements of 3x3 grid
-        int[] borderIndices = new int[] { 0, 1, 2, 5, 8, 7, 6, 3 };
-        for (int i : borderIndices) {
-            int num = grid[row + i / 3][col + (i % 3)];
-            border.append(num);
+        
+        for(int i=0;i<3;i++){ // row and col sum checking
+            if((grid[r][c+i] + grid[r+1][c+i] + grid[r+2][c+i]) != sum) return false;
+            if(grid[r+i][c] + grid[r+i][c+1] + grid[r+i][c+2] != sum) return false;
         }
 
-        String borderConverted = border.toString();
+        //diagonal sum checking
+        if((grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2]) != sum) return false;
+        if(grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c] != sum) return false;
+        return true;
+    }
 
-        // Make sure the sequence starts at one of the corners
-        return (
-            grid[row][col] % 2 == 0 &&
-            (sequence.contains(borderConverted) ||
-                sequenceReversed.contains(borderConverted)) &&
-            grid[row + 1][col + 1] == 5
-        );
+    public int numMagicSquaresInside(int[][] grid) {
+        int m = grid.length,n = grid[0].length;
+        int c = 0;
+        for(int i=0;i<=m-3;i++){
+            for(int j=0;j<=n-3;j++){
+                if(isMagicSquare(grid,i,j)) c++;
+            }
+        }
+        return c;
     }
 }
