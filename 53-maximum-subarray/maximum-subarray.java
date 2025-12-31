ac6eead025a50d5b1ee42a0238df4391c496e3d4
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2025-12-31 19:42:12 +0530

    Time: 1 ms (99.79%) | Memory: 77.2 MB (45.72%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int minValue = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > minValue) {
+                minValue = sum;
+            }
+            if (sum < 0){
+                sum = 0;
+            }
+        }
+        return minValue;
+    }
+}
