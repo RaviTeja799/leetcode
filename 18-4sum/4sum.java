9ab2b3d3706d3a467512c5e9511e5766f0c6c2af
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2026-01-13 19:55:28 +0530

    Time: 19 ms (77.71%) | Memory: 45.8 MB (51.78%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -2,20 +2,32 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                Set<Long> hashset = new HashSet<>();
-                for (int k = j + 1; k < n; k++) {
-                    long sum = (long) nums[i] + nums[j] + nums[k];
-                    long val = (long) target - sum;
-                    if (hashset.contains(val)) {
-                        List<Integer> list = Arrays.asList(nums[i], nums[j], nums[k], (int) val);
-                        Collections.sort(list);
-                        if (!ans.contains(list))
-                            ans.add(list);
-                    }
-                    hashset.add((long) nums[k]);
+        Arrays.sort(nums);
 
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int left = j + 1, right = n - 1;
+
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        int lVal = nums[left], rVal = nums[right];
+                        while (left < right && nums[left] == lVal) left++;
+                        while (left < right && nums[right] == rVal) right--;
+                    } 
+                    else if (sum < target) {
+                        left++;
+                    } 
+                    else {
+                        right--;
+                    }
                 }
             }
         }
