a14fdc7044c7c50394d17d3627b56eeedb6fcbf9
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2026-01-13 07:32:41 +0530

    Time: 25 ms (12.90%) | Memory: 76.7 MB (91.51%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        boolean ans = false;
+        Arrays.sort(nums);
+        for(int i = 0 ; i < nums.length-1 ; i++){
+            if(nums[i]==nums[i+1]){
+                ans = true;
+            }
+        }
+        return ans;
+    }
+}
