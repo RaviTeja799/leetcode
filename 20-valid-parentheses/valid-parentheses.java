c35ac4a8622c0596edd35cba2cf2f00687281fad
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2026-03-06 12:57:29 +0530

    Time: 3 ms (87.51%) | Memory: 43.3 MB (57.33%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int n = s.length();
+        for(int i = 0; i < n; i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }
+            else if (ch == ')'){
+                if(!stack.isEmpty() && stack.peek() == '('){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else if (ch == '}'){
+                if(!stack.isEmpty() &&stack.peek() == '{'){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else if (ch == ']'){
+                if(!stack.isEmpty() &&stack.peek() == '['){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+
+        }
+        if(stack.isEmpty()) return true;
+        return false;
+    }
+}
