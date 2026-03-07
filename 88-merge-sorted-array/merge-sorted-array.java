8f48eff49aad245cfbe8a918641d1f203cbec989
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2026-03-07 18:17:16 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (24.99%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int arr[] = new int[m+n];
+        int i = 0,j = 0,k = 0;
+        while(i < nums1.length-n && j < n){
+            if(nums1[i]<nums2[j]){
+                arr[k++]=nums1[i++];
+            }
+            else{
+                arr[k++]=nums2[j++];
+            }
+        }
+        if(i<nums1.length-n){
+            while(i<m) arr[k++]=nums1[i++];
+        }
+        if(j<n) while(j<n) arr[k++]=nums2[j++];
+        for(i = 0; i < nums1.length; i++) nums1[i]=arr[i];
+    }
+}
