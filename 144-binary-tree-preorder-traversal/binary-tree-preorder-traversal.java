4ef0b86e7a4d593aa8c2df5db3bb07b4fdd1bf30
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2026-03-09 16:26:42 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (92.46%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -1,45 +1,19 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    // public void helper (TreeNode root,List<Integer> list) {
-    //     if(root == null ) return;
-    //     list.add(root.val);
-    //     helper(root.left,list);
-    //     helper(root.right,list);
-    // }
     public List<Integer> preorderTraversal(TreeNode root) {
-    // List<Integer> list =  new ArrayList<>();
-    //     helper(root,list);
-    //     return list;
         List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+
         Stack<TreeNode> stack = new Stack<>();
-        TreeNode curr = root;
-        while(curr!=null || !stack.empty()){
-            while(curr!=null){
-                list.add(curr.val); 
-                if(curr.right!= null) stack.push(curr.right);
-                curr = curr.left;
-            }
-            if(!stack.empty()){
-                TreeNode node = stack.pop();
-                if(node.right!=null) stack.add(node.right);
-                list.add(node.val);
-                curr = node.left;
-            }
+        stack.push(root);
+
+        while(!stack.empty()){
+            TreeNode node = stack.pop();
+            list.add(node.val);
+
+            if(node.right != null) stack.push(node.right);
+            if(node.left != null) stack.push(node.left);
         }
+
         return list;
     }
 }
