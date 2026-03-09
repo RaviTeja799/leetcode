8b29b7c944b955b1938bae1cf61e55e8a7abf6e2
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2026-03-09 16:12:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (98.15%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -14,15 +14,32 @@
  * }
  */
 class Solution {
-    public void helper (TreeNode root,List<Integer> list) {
-        if(root == null ) return;
-        list.add(root.val);
-        helper(root.left,list);
-        helper(root.right,list);
-    }
+    // public void helper (TreeNode root,List<Integer> list) {
+    //     if(root == null ) return;
+    //     list.add(root.val);
+    //     helper(root.left,list);
+    //     helper(root.right,list);
+    // }
     public List<Integer> preorderTraversal(TreeNode root) {
-    List<Integer> list =  new ArrayList<>();
-        helper(root,list);
+    // List<Integer> list =  new ArrayList<>();
+    //     helper(root,list);
+    //     return list;
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while(curr!=null || !stack.empty()){
+            while(curr!=null){
+                list.add(curr.val); 
+                if(curr.right!= null) stack.push(curr.right);
+                curr = curr.left;
+            }
+            if(!stack.empty()){
+                TreeNode node = stack.pop();
+                if(node.right!=null) stack.add(node.right);
+                list.add(node.val);
+                curr = node.left;
+            }
+        }
         return list;
     }
 }
