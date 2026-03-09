09b05275485a8fec71f21f859c36421f2cddfc84
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2026-03-09 13:17:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (90.00%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -2,11 +2,34 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(matrix[i][j] == target) return true;
+
+        int lowr = 0, highr = m - 1;
+
+        // Find the possible row
+        while (lowr <= highr) {
+            int midr = lowr + (highr - lowr) / 2;
+
+            if (target < matrix[midr][0]) {
+                highr = midr - 1;
+            } 
+            else if (target > matrix[midr][n - 1]) {
+                lowr = midr + 1;
+            } 
+            else {
+                // target must be in this row
+                int lowc = 0, highc = n - 1;
+
+                while (lowc <= highc) {
+                    int midc = lowc + (highc - lowc) / 2;
+
+                    if (matrix[midr][midc] == target) return true;
+                    else if (matrix[midr][midc] < target) lowc = midc + 1;
+                    else highc = midc - 1;
+                }
+                return false;
             }
         }
+
         return false;
     }
 }
