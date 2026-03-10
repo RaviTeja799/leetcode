bec5411e6b53a5847fb774ad5bf5a58d0e4ab00a
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2026-03-10 13:23:50 +0530

    Time: 2 ms (4.37%) | Memory: 45.2 MB (27.12%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -1,15 +1,36 @@
+class Element{
+    int sum;
+    TreeNode node;
+    Element(TreeNode node , int sum){
+        this.sum = sum;
+        this.node = node;
+    }
+}
+
+
+
 class Solution {
 
-    public boolean helper(TreeNode root, int sum, int targetSum) {
-        if (root == null) return false;
-        sum += root.val;
-        if (root.left == null && root.right == null) {
-            return sum == targetSum;
-        }
-        return helper(root.left, sum, targetSum) || helper(root.right, sum, targetSum);
-    }
+    // public boolean helper(TreeNode root, int sum, int targetSum) {
+    //     if (root == null) return false;
+    //     sum += root.val;
+    //     if (root.left == null && root.right == null) {
+    //         return sum == targetSum;
+    //     }
+    //     return helper(root.left, sum, targetSum) || helper(root.right, sum, targetSum);
+    // }
 
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        return helper(root, 0, targetSum);
+        // return helper(root, 0, targetSum);
+        if(root == null) return false;
+        Deque<Element> dq = new ArrayDeque<>();
+        dq.offer(new Element(root, root.val));
+        while(!dq.isEmpty()){
+            Element curr = dq.poll();
+            if(curr.node.left == null && curr.node.right == null && curr.sum == targetSum) return true;
+            if(curr.node.left != null) dq.offer(new Element(curr.node.left, curr.sum+curr.node.left.val));
+            if(curr.node.right != null) dq.offer(new Element(curr.node.right, curr.sum+curr.node.right.val));
+        }
+        return false;
     }
 }
