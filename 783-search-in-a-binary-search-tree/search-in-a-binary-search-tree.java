6be16a99c3c8f30cbb0b643cb48246c41ae891e7
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2026-03-10 15:01:30 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (51.21%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -15,15 +15,10 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if (root == null) {
-            return null;
-        }
-        if (root.val == val) {
-            return root;
-        } else if (root.val > val) {
-            return searchBST(root.left, val);
-        } else {
-            return searchBST(root.right, val);
-        }        
+        if(root==null)return null;
+        if(root.val==val)return root;
+        if(val>root.val)return searchBST(root.right,val);
+        return searchBST(root.left,val);
+
     }
 }
