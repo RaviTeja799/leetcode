06345372ab6e2a0a7257b4b62852353b8a27c5e8
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2026-03-20 11:15:20 +0530

    Time: 6 ms (99.69%) | Memory: 47.8 MB (83.33%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,30 +1,32 @@
 class Solution {
-        public int left(int [] piles, int max , int h){
-        int val = max;
-        int low = 1, high = max;
-        while(low<=high){
+    int binarySearch(int piles[], int h,int max){
+        int low = 1,high = max , ans = max;
+        while(low <= high){
             int mid = low+(high-low)/2;
-            if(check(piles,mid) <= h){
-                val = mid;
+            if(check(piles,mid)<=(long)h){
+                ans = mid;
                 high = mid - 1;
             }
-            else low = mid + 1;
+            else{
+                low = mid + 1;
+            }
         }
-        return val;
+        return ans;
     }
-    public long check(int piles[] , int mid){
-        long cnt = 0;
-        for(int i = 0; i < piles.length; i++){
-            cnt += Math.ceil((double)piles[i] / mid);
+    public long check(int[] piles, int mid) {
+        int n = piles.length;
+        long count = 0;
+        for(int i = 0; i < n; i++){
+            count+= (piles[i]+mid-1)/mid;
         }
-        return cnt;
-    }       
+        return count;
+    }
     public int minEatingSpeed(int[] piles, int h) {
+        int n = piles.length;
         int max = piles[0];
-        for(int i = 0; i < piles.length; i++){
-            max = Math.max(max,piles[i]);
+        for(int i = 0; i < n; i++){
+            max = Math.max(piles[i],max);
         }
-        return left(piles,max,h);
-
+        return binarySearch(piles,h,max);
     }
 }
