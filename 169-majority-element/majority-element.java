a9bca5b185526bc9e2d8e08dfd9984b7285779ef
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2026-03-24 13:27:25 +0530

    Time: 7 ms (43.18%) | Memory: 55.8 MB (43.40%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,21 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i] , map.getOrDefault(nums[i] , 0)+1);
-        }
-        int val = nums[0];
-        int freq = map.get(val);
-        for(int i = 0; i < nums.length; i++){
-            if(map.get(nums[i]) > freq){
-                freq = map.get(nums[i]);
-                val = nums[i];
-            }
-        }
+        // HashMap
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int i = 0; i < nums.length; i++){
+        //     map.put(nums[i] , map.getOrDefault(nums[i] , 0)+1);
+        // }
+        // int val = nums[0];
+        // int freq = map.get(val);
+        // for(int i = 0; i < nums.length; i++){
+        //     if(map.get(nums[i]) > nums.length/2){
+        //         val = nums[i];
+        //     }
+        // }
+
+        // Sorting
+        Arrays.sort(nums);
+        int val = nums[nums.length/2];
         return val;
     }
 }
