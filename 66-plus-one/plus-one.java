ec184347aac009243349b777067eaece9875450c
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2026-03-24 13:16:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (87.10%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for(int i = digits.length - 1; i >= 0; i--) {
-            if(digits[i] != 9) {
-                digits[i] += 1;
-                return digits;
-            }
-            digits[i] = 0;
-        }   
-        digits = new int[digits.length + 1];
-        digits[0] = 1;
+        int n = digits.length;
+        int i = n - 1;
+        while(i>=0 && digits[i]==9){
+            digits[i]=0;
+            i--;
+        }
+        if(i == -1){
+            int ans [] = new int[n+1];
+            ans[0] =1 ;
+            return ans;
+        }
+        digits[i]++;
         return digits;
     }
 }
