b4926d95819cecb1af64c906e5768216ab19378e
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2026-03-25 18:08:48 +0530

    Time: 11 ms (90.82%) | Memory: 46.6 MB (97.82%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,20 +1,25 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Stack<Integer> stk = new Stack<>();
+        // Stack<Integer> stk = new Stack<>();
+        int open = 0,close = 0;
         StringBuilder sb = new StringBuilder(s);
-        for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
+        for(int i = 0; i < sb.length(); i++){
+            char ch = sb.charAt(i);
+
             if(ch == '('){
-                stk.push(i);
+                open++;
             }
             else if(ch == ')'){
-                if(stk.isEmpty() || s.charAt(stk.peek()) == ')') stk.push(i);
-                else if(s.charAt(stk.peek()) == '(') stk.pop();
+                if(open > 0) open--;
+                else {sb.deleteCharAt(i);i--;}
             }
-
         }
-        while(!stk.isEmpty()){
-            sb.deleteCharAt(stk.pop());
+        System.out.println(sb);
+        for(int i = sb.length()-1;open >0; i--){
+            if(sb.charAt(i)=='(') {
+                sb.deleteCharAt(i);
+                open--;
+            }
         }
         return sb.toString();
     }
