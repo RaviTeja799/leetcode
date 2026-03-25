31ad7abd62a32324e2afcf9722dfe02c38aa4bfc
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2026-03-25 16:29:44 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (83.66%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,11 @@
 class Solution {
-    public double myPow(double x, int n) {
-        return Math.pow(x,n);
+    public double myPow(double x, long n){
+        if(n == 0) return 1;
+        if(n<0) {
+            x = 1/x;
+            n = -n;
+        }   
+        if(n%2 != 0) return x*myPow(x*x,n/2);
+        else return myPow(x*x,n/2);
     }
 }
