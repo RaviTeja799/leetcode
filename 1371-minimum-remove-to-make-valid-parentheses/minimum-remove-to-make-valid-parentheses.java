f8cadd402057b9ba28bc19164dc5a5d4031d3672
RAVI TEJA BHAGAVATULA <bhraviteja799@gmail.com>
2026-03-25 18:10:33 +0530

    Time: 12 ms (89.29%) | Memory: 46.3 MB (99.42%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -2,7 +2,7 @@ class Solution {
     public String minRemoveToMakeValid(String s) {
         // Stack<Integer> stk = new Stack<>();
         int open = 0,close = 0;
-        StringBuilder sb = new StringBuilder(s);
+        StringBuffer sb = new StringBuffer(s);
         for(int i = 0; i < sb.length(); i++){
             char ch = sb.charAt(i);
 
@@ -14,7 +14,6 @@ class Solution {
                 else {sb.deleteCharAt(i);i--;}
             }
         }
-        System.out.println(sb);
         for(int i = sb.length()-1;open >0; i--){
             if(sb.charAt(i)=='(') {
                 sb.deleteCharAt(i);
